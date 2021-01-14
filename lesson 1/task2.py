e175e9043a1f635f8631fc7246447eda5fc82b73
Aleksandr <mail.privalov@gmail.com>
2021-01-14 16:01:35 +0300

    123

--- lesson 1/task2.py
+++ lesson 1/task2.py
@@ -2,4 +2,4 @@ time = int(input('Введите время в секундах: '))
 seconds = time % 60
 minutes = time // 60
 hours = time // 3600
-print(f"чч:мм:сс   {hours} : {minutes % 60:02} : {seconds:02}")
+print(f'{hours} : {minutes % 60:02} : {seconds:02}')
